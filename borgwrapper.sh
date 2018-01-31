e5b037062cb099e161137b88aa8674a78ba0320c
Robert Schindler <r.schindler@efficiosoft.com>
2018-01-31 18:01:32 +0100

    Removed obsolete borgwrapper.sh script

--- borgwrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo "You are using the old borgwrapper.sh script, please change"
-echo "borgwrapper.sh to backwrap."
-echo
-
-script="$(dirname "$0")/backwrap"
-exec "$script" "$@"
